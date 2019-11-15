6d8a36b18415ee2c4c3c61307408607efa278775
NoMaD38 <nomad38note@gmail.com>
2019-11-15 22:41:59 +0800

    finish 10.1

--- lab10/pattern.js
+++ lab10/pattern.js
@@ -1,59 +1,132 @@
 const fs = require("fs")
 
+const buff = new Buffer.alloc(10)
+
 let read = fs.readFile("source.txt", "utf8", 
             (error,data) => {
                 console.log("чтение файла");
-                if(error) throw error // если возникла ошибка
-                const lex = new Parser(data)
-                lex.output_tree()
-
-});
-
-class Lexer{
+                if(error) throw error 
+                new Creator(data)
+            })
+class Regular{
     constructor(data){
-        this.data
+        this.data = data
+        this.regular()
     }
-    table_lex(){
-        console.log('таблица лексем');
+    regular () {
+        let strings = this.data.replace(/\s{2,}/g, ' ')
+        console.log('регурные выражения');
+        return strings
     }
-    table_ident(){
-        console.log('таблица идентификаторов');
+}
+class Creator{
+    constructor(data){
+        this.data = data
+        const lexer = new Lexer(this.data)
+        const parser = new Parser(lexer.data_lex, lexer.data_ident)
+        const optimizator = new Optimizator(parser.tree, lexer.data_ident)
+        const obj_generator = new Generator(optimizator.tern)
+ }
+}
+class Lexer {
+    constructor(data){
+        this.data = data
+        const data_reg = new Regular(this.data)
+        const data_lex = new Table_lex(data_reg)
+        const data_ident = new Table_ident(data_reg)
+    }
+}
+class Table_lex {
+    constructor(data){
+        this.data = data
+        this.table()
+    }
+    table(){
+        console.log('тут должна быть таблица идентификаторов')
+    }
+}
+class Table_ident  {
+    constructor(data){
+        this.data = data
+        this.table()
+    }
+    table(){
+        console.log('тут должна быть таблица идентификаторов');
     }
 }
 
-class Parser{
-    constructor(table_lex,table_ident){
-        this.table_ident
-        this.table_lex
+class Parser {
+    constructor(data_lex,data_ident){
+        this.data_lex = data_lex
+        this.data_ident = data_ident
+        const check = new Check_error()
+        const tree = new Output_tree()
     }
-    check_error(){
-        console.log('получаем ошибки');
+}
+
+class Check_error{
+    constructor(data){
+        this.data = data
+        this.error(this.data)
     }
-    output_tree(){
-        console.log('дерево вывода грамматики');
+    error(data){
+        console.log('тут должна быть проверка на ошибки');
+    }
+}
+
+class Output_tree{
+    constructor(data){
+        this.data = data
+        this.tree(this.data)
+    }
+    tree(data){
+        console.log('тут должно быть дерево вывода');
     }
 }
 
 class Optimizator{
-    constructor(output_tree, table_ident){
-        this.output_tree
-        this.table_ident
+    constructor(output_tree, data_ident){
+    this.output_tree = output_tree
+    this.data_ident = data_ident
+    const tern = new Ternary()
+    const opt = new Optimization()
     }
+}
 
-    ternary(){
-        console.log('строим триады');
+class Ternary{
+    constructor(data){
+        this.data = data
+        this.ternars(this.data)
     }
-    optimization(){
-        console.log('проводим оптимизацию');
+    ternars(data){
+        console.log('тут должнЫ быть триады');
+    }
+}
+
+class Optimization{
+    constructor(data){
+        this.data = data
+        this.opt(this.data)
+    }
+    opt(data){
+        console.log('тут должна быть проведена оптимизация');
     }
 }
 
 class Generator{
     constructor(ternary){
-        this.ternary
+        this.ternary = ternary
+        const obj_code = new Generation(this.ternary)
     }
+}
 
-    generation(){
-        console.log('генерация объектного кода');
+class Generation{
+    constructor(data){
+        this.data = data
+        this.generator(this.data)
+    }
+    generator(data){
+        buff = data 
+        console.log('генерация объектного кода и занесение в буфер');
     }
 }
